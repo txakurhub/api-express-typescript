99f9b803afe39484a05f4208cc4332d8c98a4b3c
Leandro Pereyra <me.leandrop@gmail.com>
2022-12-16 19:03:01 -0300

    express types

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express from "express"
+
+const app = express()
+
+app.use(express.json())
+
+const PORT = 3000
+
+app.get("/ping", (req, res) => {
+    console.log("Someone pinged here")
+    res.send('Pong!')
+})
+
+app. listen(PORT, () => {
+    console.log(`Server running on port: ${PORT}`)
+})
